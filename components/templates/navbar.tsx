c93569c0b1f7cc8057dc646640d412197dd1a25d
Rifat Khan <rifat6633@gmail.com>
2024-01-24 21:03:37 +0600

    add user profile and sign in dropdown

--- components/templates/navbar.tsx
+++ components/templates/navbar.tsx
@@ -1,15 +1,57 @@
 import Logo from "@/components/uis/logo";
 import { ModeToggle } from "./dark-mode";
+import { Separator } from "@/components/shadcn-ui/separator";
+import { Button } from "@/components/shadcn-ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/shadcn-ui/dropdown-menu";
+import { PersonIcon } from "@radix-ui/react-icons";
+import Link from "next/link";
+import { auth } from "@clerk/nextjs";
+import UserAvatar from "./user-avatar";
 
 export default function Navbar() {
+  const user = auth();
+
   return (
     <header className="py-5 container flex justify-between items-center">
       <div>
         <Logo variants="link" />
       </div>
 
-      <div>
+      <div className="h-8 flex items-center gap-2">
         <ModeToggle />
+
+        <Separator orientation="vertical" />
+
+        {user.userId ? (
+          <UserAvatar />
+        ) : (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="icon">
+                <PersonIcon />
+              </Button>
+            </DropdownMenuTrigger>
+
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem asChild>
+                <Link href={"/sign-in"} className="cursor-pointer">
+                  Sign In
+                </Link>
+              </DropdownMenuItem>
+
+              <DropdownMenuItem asChild>
+                <Link href={"/sign-up"} className="cursor-pointer">
+                  Create an account
+                </Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
       </div>
     </header>
   );
