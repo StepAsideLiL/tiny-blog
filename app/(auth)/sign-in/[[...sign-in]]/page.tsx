c646e3be0d9a99253a9632c3cae873df21a1bb04
Rifat Khan <rifat6633@gmail.com>
2024-01-24 20:22:48 +0600

    create sign-in and sign-up page

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { SignIn } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import { useTheme } from "next-themes";
+
+export default function SignInPage() {
+  const { theme } = useTheme();
+
+  return (
+    <>
+      <SignIn
+        appearance={{
+          baseTheme: theme === "dark" ? dark : undefined,
+        }}
+      />
+    </>
+  );
+}
