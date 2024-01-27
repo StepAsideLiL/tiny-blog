98ead5e3b5bc493e7db4a5d02011dd2e6a152790
Rifat Khan <rifat6633@gmail.com>
2024-01-27 15:58:28 +0600

    design: responsive

--- app/(web)/_parts/home-page-sections.tsx
+++ app/(web)/_parts/home-page-sections.tsx
@@ -16,74 +16,76 @@ export default async function HomePageSections() {
   const blogs = await getBlogs();
 
   return (
-    <section className="grid grid-cols-12">
+    <section className="w-full lg:grid lg:grid-cols-12 flex justify-center">
       {/* User Profile */}
-      <section className="md:col-span-3 col-span-2">Profile</section>
+      <section className="col-span-3 hidden lg:block">Profile</section>
 
       {/* Tyni Blog List */}
-      <section className="md:col-span-6 col-span-8 border-x border-muted divide-y border-b">
-        {userId ? (
-          <BlogForm userId={userId} />
-        ) : (
-          <div className="p-2 text-center">
-            <Button variant={"outline"} asChild>
-              <Link href={"/sign-up"}>Sign up to write a blog</Link>
-            </Button>
-          </div>
-        )}
+      <section className="max-w-lg w-[512px] lg:max-w-5xl lg:w-full lg:col-span-6 col-span-12 border-x border-muted divide-y border-b">
+        <section>
+          {userId ? (
+            <BlogForm userId={userId} />
+          ) : (
+            <div className="p-2 text-center">
+              <Button variant={"outline"} asChild>
+                <Link href={"/sign-up"}>Sign up to write a blog</Link>
+              </Button>
+            </div>
+          )}
 
-        {blogs.length !== 0 ? (
-          blogs.map(async (blog) => {
-            const user = await clerkClient.users.getUser(blog.userId);
-            const posted = formatDistanceToNow(new Date(blog.createdAt));
+          {blogs.length !== 0 ? (
+            blogs.map(async (blog) => {
+              const user = await clerkClient.users.getUser(blog.userId);
+              const posted = formatDistanceToNow(new Date(blog.createdAt));
 
-            return (
-              <article key={blog.id} className="p-3 space-y-2">
-                <div className="flex gap-2 items-center">
-                  <Avatar>
-                    <AvatarFallback>
-                      {user.firstName ? user.firstName[0].toUpperCase() : ""}
-                    </AvatarFallback>
-                    <AvatarImage src={user.imageUrl} />
-                  </Avatar>
+              return (
+                <article key={blog.id} className="p-3 space-y-2">
+                  <div className="flex gap-2 items-center">
+                    <Avatar>
+                      <AvatarFallback>
+                        {user.firstName ? user.firstName[0].toUpperCase() : ""}
+                      </AvatarFallback>
+                      <AvatarImage src={user.imageUrl} />
+                    </Avatar>
 
-                  <div>
-                    <h1 className="text-sm font-medium">
-                      {user.firstName
-                        ? `${user.firstName} ${user.lastName}`
-                        : `${user.username}`}
-                    </h1>
-                    <p className="text-sm text-muted-foreground">
-                      {user.username} &bull; {posted}
-                    </p>
+                    <div>
+                      <h1 className="text-sm font-medium">
+                        {user.firstName
+                          ? `${user.firstName} ${user.lastName}`
+                          : `${user.username}`}
+                      </h1>
+                      <p className="text-sm text-muted-foreground">
+                        {user.username} &bull; {posted}
+                      </p>
+                    </div>
                   </div>
-                </div>
 
-                <div className="flex gap-2 items-center">
-                  <div className="w-10"></div>
+                  <div className="flex gap-2 items-center">
+                    <div className="w-10"></div>
 
-                  <div className="flex-1">
-                    <Separator orientation="horizontal" />
+                    <div className="flex-1">
+                      <Separator orientation="horizontal" />
 
-                    <h1 className="font-medium">{blog.title}</h1>
+                      <h1 className="font-medium">{blog.title}</h1>
 
-                    <p className="font-light whitespace-pre-line">
-                      {blog.body}
-                    </p>
+                      <p className="font-light whitespace-pre-line">
+                        {blog.body}
+                      </p>
+                    </div>
                   </div>
-                </div>
-              </article>
-            );
-          })
-        ) : (
-          <h1 className="py-3 text-center text-muted-foreground">
-            No Blogs Found
-          </h1>
-        )}
+                </article>
+              );
+            })
+          ) : (
+            <h1 className="py-3 text-center text-muted-foreground">
+              No Blogs Found
+            </h1>
+          )}
+        </section>
       </section>
 
       {/* Blog info and Reply */}
-      <section className="md:col-span-3 col-span-2">Reply</section>
+      <section className="col-span-3 hidden lg:block">Reply</section>
     </section>
   );
 }
