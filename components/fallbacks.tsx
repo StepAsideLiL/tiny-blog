e56efff3496685644ac81d1fbf5c8a977c5c5599
Rifat Khan <rifat6633@gmail.com>
2024-01-28 23:31:21 +0600

    create: fallback component UserBlogsFallback in dedicated file

---
+++ components/fallbacks.tsx
@@ -0,0 +1,7 @@
+export function UserBlogsFallback() {
+  return (
+    <div className="flex justify-center items-center">
+      <h1 className="text-muted-foreground text-lg">No Blogs Posted</h1>
+    </div>
+  );
+}
