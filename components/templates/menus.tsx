e6dec88b8a6efc21f94d918d90476ce021aa3616
Rifat Khan <rifat6633@gmail.com>
2024-01-28 21:22:27 +0600

    add: username in profile link

--- components/templates/menus.tsx
+++ components/templates/menus.tsx
@@ -1,33 +1,35 @@
 "use client";
 
 import { cn } from "@/lib/utils";
+import { useUser } from "@clerk/nextjs";
 import { Home, NotebookPen, User } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
-const menuList = [
-  {
-    title: "Home",
-    href: "/",
-    icon: <Home strokeWidth="0.5px" size={"30px"} />,
-    activeIcon: <Home strokeWidth="3px" size={"30px"} />,
-  },
-  {
-    title: "Write Blog",
-    href: "/add",
-    icon: <NotebookPen strokeWidth="0.5px" size={"30px"} />,
-    activeIcon: <NotebookPen strokeWidth="3px" size={"30px"} />,
-  },
-  {
-    title: "Profile",
-    href: "/profile",
-    icon: <User strokeWidth="0.5px" size={"30px"} />,
-    activeIcon: <User strokeWidth="3px" size={"30px"} />,
-  },
-];
-
 export default function Menus({ className = "" }: { className?: string }) {
   const pathname = usePathname();
+  const { user } = useUser();
+
+  const menuList = [
+    {
+      title: "Home",
+      href: "/",
+      icon: <Home strokeWidth="0.5px" size={"30px"} />,
+      activeIcon: <Home strokeWidth="3px" size={"30px"} />,
+    },
+    {
+      title: "Write Blog",
+      href: "/add",
+      icon: <NotebookPen strokeWidth="0.5px" size={"30px"} />,
+      activeIcon: <NotebookPen strokeWidth="3px" size={"30px"} />,
+    },
+    {
+      title: "Profile",
+      href: `/${user?.username}`,
+      icon: <User strokeWidth="0.5px" size={"30px"} />,
+      activeIcon: <User strokeWidth="3px" size={"30px"} />,
+    },
+  ];
 
   return (
     <nav className={cn("flex gap-5", className)}>
