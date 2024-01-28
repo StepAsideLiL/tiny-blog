57c82aafb8550bcba792c1b5242297d3021c204f
Rifat Khan <rifat6633@gmail.com>
2024-01-29 00:41:42 +0600

    add: UserBlogs component

--- app/(profile)/u/[username]/page.tsx
+++ app/(profile)/u/[username]/page.tsx
@@ -1,3 +1,7 @@
+import { Suspense } from "react";
+import UserBlogs from "./_parts/user-blogs";
+import { UserBlogsFallback } from "@/components/fallbacks";
+
 export default function UsernamePage({
   params,
 }: {
@@ -6,6 +10,9 @@ export default function UsernamePage({
   return (
     <main className="container">
       <h1>{params.username}</h1>
+      <Suspense fallback={<UserBlogsFallback />}>
+        <UserBlogs username={params.username} />
+      </Suspense>
     </main>
   );
 }
