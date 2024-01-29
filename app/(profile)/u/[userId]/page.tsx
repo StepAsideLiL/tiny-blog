330c5558b611d99e5638dc0b3e9ebd2efc5b2f91
Rifat Khan <rifat6633@gmail.com>
2024-01-29 11:02:26 +0600

    change: to userId dynamic page

---
+++ app/(profile)/u/[userId]/page.tsx
@@ -0,0 +1,22 @@
+import { Suspense } from "react";
+import UserBlogs from "./_parts/user-blogs";
+import { UserBlogsFallback, UserProfileFallback } from "@/components/fallbacks";
+import UserProfile from "./_parts/user-profile";
+
+export default function UsernamePage({
+  params,
+}: {
+  params: { userId: string };
+}) {
+  return (
+    <main className="container">
+      <Suspense fallback={<UserProfileFallback />}>
+        <UserProfile userId={params.userId} />
+      </Suspense>
+
+      <Suspense fallback={<UserBlogsFallback />}>
+        <UserBlogs userId={params.userId} />
+      </Suspense>
+    </main>
+  );
+}
